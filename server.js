a32504cf77fe1f7855122cd5209d327024e9bc2c
jrmcgarvey <jmcgarve@bellsouth.net>
2022-10-04 13:11:26 -0400

    prettier and eslint

--- server.js
+++ server.js
@@ -1,38 +1,44 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const session = require('express-session')
-require('dotenv').config()
-const Task = require('./models/Task')
-const taskRouter = require('./routes/tasks')
-const setMessage = require('./middleware/message')
-const connectDB = require('./db/connect')
+const session = require("express-session");
+require("dotenv").config();
+const Task = require("./models/Task");
+const taskRouter = require("./routes/tasks");
+const setMessage = require("./middleware/message");
+const connectDB = require("./db/connect");
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
-app.use(express.urlencoded({extended: false}))
-app.use('/tasks', setMessage, taskRouter)
+app.set("view engine", "ejs");
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use("/tasks", setMessage, taskRouter);
 // use res.render to load up an ejs view file
 // index page
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
   var mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-    { name: 'Tux', organization: "Linux", birth_year: 1996},
-    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
+    { name: "Tux", organization: "Linux", birth_year: 1996 },
+    { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
   ];
-  var tagline = "No programming concept is complete without a cute animal mascot.";
+  var tagline =
+    "No programming concept is complete without a cute animal mascot.";
 
-  res.render('pages/index', {
+  res.render("pages/index", {
     mascots: mascots,
-    tagline: tagline
+    tagline: tagline,
   });
 });
 
 // about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+app.get("/about", function (req, res) {
+  res.render("pages/about");
 });
-
 
 //app.listen(8080);
 //console.log('Server is listening on port 8080');
